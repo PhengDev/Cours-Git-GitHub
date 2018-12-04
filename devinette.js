37c394ec7d808a025720b7294ca1796cd21edbfc
PhengDev <45602136+PhengDev@users.noreply.github.com>
2018-12-04 19:53:59 +0100

    Ajout des difficultés de niveau

--- devinette.js
+++ devinette.js
@@ -1,4 +1,10 @@
 alert("Bienvenue dans ce jeu de devinette !");
 
     var solution = Math.floor(Math.random() * 100) + 1;
-  
+    var difficulter = "";
+
+    while ((difficulter !== "facile") && (difficulter !== "normal") && (difficulter !== "difficile"))
+    {
+    	difficulter = prompt("Choissisez la difficulter: " + "\n facile / normal / difficile");
+        difficulter = difficulter.toLowerCase();
+    }
